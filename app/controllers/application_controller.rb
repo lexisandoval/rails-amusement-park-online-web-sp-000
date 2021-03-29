b144cd0fb8daf31f506b70a0d489c292a00d3e97
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-29 17:09:16 -0500

    commit tree d384f91939a1244489d8acdb55f89ac08f57b2d4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+
+  helper_method :current_user
+    
+  def current_user 
+    @current_user ||= User.find_by(id: session[:user_id])
+  end 
+
+  private 
+  
+  def require_login
+    redirect_to root_path unless current_user
+  end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+    before_save :admin?
+    
+    def admin?
+        self.admin ||= false 
+    end 
+
+    def mood 
+        if self.happiness && self.nausea
+            if self.happiness > self.nausea
+                mood = "happy"
+            else 
+                mood = "sad"
+            end 
+        end 
+    end 
 end
